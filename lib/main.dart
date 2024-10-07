de5e79ad331caed5cedf0a3a385d56ac830f0e8a
John Pham <johnpham290@yahoo.com>
2024-10-07 18:50:11 -0400

    added animate button

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,42 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   int _counter = 0;
+  bool _isTextVisible = false; // Track visibility of animated text
+  late AnimationController _controller; // Animation controller
+  late Animation<double> _scaleAnimation; // Scale animation
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _toggleText() {
+    setState(() {
+      _isTextVisible = !_isTextVisible;
+      if (_isTextVisible) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
     });
   }
 
@@ -49,21 +79,44 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/images/halloween.jpg'), // Moved here as a child
+            Image.asset('assets/images/halloween.jpg'), // child
             const Text(
-              'You have pushed the button this many times:',
+              'TRAPS SET OFF:',
+              style: TextStyle(color: Colors.orange), // Set text color to orange
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.orange), // Set text color to orange
+            ),
+            // Animated text
+            ScaleTransition(
+              scale: _scaleAnimation,
+              child: Opacity(
+                opacity: _isTextVisible ? 1.0 : 0.0,
+                child: const Text(
+                  'XD Happy Halloween! T_T\n YOU FOUND IT!!!',
+                  style: TextStyle(fontSize: 24, color: Colors.orange), // Text style
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 16), // Space between buttons
+          FloatingActionButton(
+            onPressed: _toggleText, // Button to toggle animated text
+            tooltip: 'Show/Hide Animated Text',
+            child: const Icon(Icons.text_fields),
+          ),
+        ],
       ),
     );
   }
